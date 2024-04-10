d5cd02ef45fc7442e19c26950511d777fb1ed1f7
Yassin Zairi <yassinzairi0@gmail.com>
2024-04-10 14:11:15 +0200

    nav bar

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/pages/home';
+import Properties from './src/pages/properties';
+import Contact from './src/pages/contact';
+import Stylesheets from './src/styles/style';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Properties" component={Properties}/>
+        <Stack.Screen name="Contact" component={Contact}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
---
+++ src/components/home/landing.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+export default function Landing(){
+    return(
+        <p>test from the landing</p>
+    )
+}
